33cf7b370bbc233be48cbd9f0c64df4bb072eb61
nikita_kogolenok <nikiton17ns@gmail.com>
2022-01-30 14:14:26 +0300

    Add Button and action in ViewController

--- AlertControllers/Controller/ViewController.swift
+++ AlertControllers/Controller/ViewController.swift
@@ -13,5 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: - Actions
+    @IBAction func buttonAction1(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonAction2(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonAction3(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonAction4(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func buttonAction5(_ sender: UIButton) {
+    }
 }
 
