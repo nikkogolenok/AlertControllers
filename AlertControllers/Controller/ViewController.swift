5ac4575028ca02a7a6e5b739483af5dd753cb433
nikita_kogolenok <nikiton17ns@gmail.com>
2022-01-30 13:57:35 +0300

    Initial Commit

---
+++ AlertControllers/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AlertControllers
+//
+//  Created by Никита Коголенок on 30.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
